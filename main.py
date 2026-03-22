079454c91beccd2124fa866b9f2a748b1f824179
JULY696 <julieta.marsan@gmail.com>
2026-03-22 23:17:17 +0000

    sd

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ from reportlab.lib.styles import getSampleStyleSheet
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
